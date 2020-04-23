de62e8a3fe456f5cc64e71ef0da5c4f3d6ea66f8
liusc <liusccc@gmail.com>
2020-04-23 12:24:36 +0800

    moew functions added to test.py

--- LNS/test.py
+++ LNS/test.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import math
 import decimal
 import subprocess
@@ -21,74 +22,40 @@ tmax = {5   :  5,
         50  : 15,
         100 : 30,
         200 : 60}
-
 grids = list(product(latin_list, ksize_list, elo_list))
 
-def config_str(config):
-    return "latin=%s_ksize=%s_elo=%d" %\
-           (config[0], str(config[1]), config[2])
-
-def test():
-    solver = "LNSr/LNS/bin/LNS"
-    insts = glob("instances/Wang_Chen/explicit_*.vrpsdptw")
-    maxParalism = 14
-
-    runningTask = 0
-    processSet = set()
-    for config in grids:
-        print('--------------------Testing %s ----------------------' % str(config))
-        for i, ins in enumerate(insts):
-            print('------------------Testing instance %d---------------' % i)
-            while True:
-                if runningTask >= maxParalism:
-                    time.sleep(0.1)
-                    finished = [
-                        pid for pid in processSet if pid.poll() is not None
-                    ]
-                    processSet -= set(finished)
-                    runningTask = len(processSet)
-                    continue
-                else:
-                    ins_id = ins[ins.rfind('_')+1:ins.find('.')]
-                    with open(ins, 'r') as f:
-                        cus_num = int(f.read().strip().split('\n')[2].split(':')[1].strip())-1
-                        t = tmax[cus_num]
-                    out_file = "results/%s_%s" % (ins_id, config_str(config))
-                    cmd = ("%s --problem %s %s --pruning --O_1_eval --two_opt --two_opt_star "
-                           "--ksize %d --init %s "
-                           "--or_opt %d --two_exchange %d --random_removal --related_removal "
-                           "--regret_insertion --greedy_insertion "
-                           "--elo %d --random_seed %d --time %d --runs %d > %s") %\
-                            (solver, ins, '--latin' if config[0] == 'on' else '',
-                             math.floor(math.sqrt(cus_num)) if config[1] == 'sqrt' else\
-                             math.floor(config[1] * cus_num),
-                             init_list[0], or_opt_list[0], two_exchange_list[0], config[2],
-                             seeds[0], t, runs, out_file)
-                    print(cmd)
-                    processSet.add(subprocess.Popen(cmd, shell=True))
-                    # pid = subprocess.Popen(cmd, shell=True)
-                    # pid.communicate()
-                    runningTask = len(processSet)
-                    break
-
-    # check if subprocess all exits
-    while processSet:
-        time.sleep(5)
-        print('Still %d sub process not exits' % len(processSet))
-        finished = [pid for pid in processSet if pid.poll() is not None]
-        processSet -= set(finished)
-
+def extract(L):
     # extract results
-    # ins_names = ["rcdp1001", "rcdp1004", "rcdp1007", "rcdp2501", "rcdp2504", "rcdp2507",\
-    #              "rcdp5001", "rcdp5004", "rcdp5007", "rdp101", "rdp102", "rdp103", "rdp104",\
-    #              "rdp105", "rdp106", "rdp107", "rdp108", "rdp109", "rdp110", "rdp111", "rdp112",\
-    #              "cdp101", "cdp102", "cdp103", "cdp104", "cdp105", "cdp106", "cdp107", "cdp108",\
-    #              "cdp109", "rcdp101", "rcdp102", \
-    #              "rcdp103", "rcdp104", "rcdp105", "rcdp106", "rcdp107", "rcdp108", "rdp201", \
-    #              "rdp202", "rdp203", "rdp204", "rdp205", "rdp206", "rdp207", "rdp208", \
-    #              "rdp209", "rdp210", "rdp211", "cdp201", "cdp202", "cdp203", "cdp204", \
-    #              "cdp205", "cdp206", "cdp207", "cdp208", "rcdp201", "rcdp202", \
-    #              "rcdp203", "rcdp204", "rcdp205", "rcdp206", "rcdp207", "rcdp208"]
+    ins_names = ["rcdp1001", "rcdp1004", "rcdp1007", "rcdp2501", "rcdp2504", "rcdp2507",\
+                 "rcdp5001", "rcdp5004", "rcdp5007", "rdp101", "rdp102", "rdp103", "rdp104",\
+                 "rdp105", "rdp106", "rdp107", "rdp108", "rdp109", "rdp110", "rdp111", "rdp112",\
+                 "cdp101", "cdp102", "cdp103", "cdp104", "cdp105", "cdp106", "cdp107", "cdp108",\
+                 "cdp109", "rcdp101", "rcdp102", \
+                 "rcdp103", "rcdp104", "rcdp105", "rcdp106", "rcdp107", "rcdp108", "rdp201", \
+                 "rdp202", "rdp203", "rdp204", "rdp205", "rdp206", "rdp207", "rdp208", \
+                 "rdp209", "rdp210", "rdp211", "cdp201", "cdp202", "cdp203", "cdp204", \
+                 "cdp205", "cdp206", "cdp207", "cdp208", "rcdp201", "rcdp202", \
+                 "rcdp203", "rcdp204", "rcdp205", "rcdp206", "rcdp207", "rcdp208"]
+    for ins_id in ins_names:
+        out_file = "results/%s_%s" % (ins_id, config_str_1(L))
+        lines = open(out_file, 'r').read().strip().split('\n')
+        for line in lines:
+            if 'DISPATCHINGCOST' in line:
+                dispatch = float(line.split(':')[-1].strip())
+                break
+        for line in reversed(lines):
+            if 'Total cost' in line:
+                total_cost = float(line.split(':')[-1].strip('. '))
+                break
+        for line in reversed(lines):
+            if 'vehicle (route) number' in line:
+                v_number = int(line.split(':')[-1].strip('. '))
+                break
+        total_cost = total_cost - dispatch * v_number
+        total_cost = float(round(decimal.Decimal(total_cost), 2))
+        print(ins_id, v_number, total_cost)
+
+def summary():
     baselines = {"rcdp1001": [3, 348.98],
                  "rcdp1004": [2, 216.69],
                  "rcdp1007": [3, 243.17],
@@ -157,14 +124,17 @@ def test():
     context = decimal.getcontext()
     context.rounding = decimal.ROUND_HALF_UP
     f = open('summary', 'w+')
-    f.write('config, [better, worse, equal, almost_equal]\n')
+    f.write('config, [v_better, v_worse, c_better, c_worse, equal], [total better,worse,equal]\n')
     for config in grids:
-        better = 0
-        worse = 0
+        v_better = 0
+        v_worse = 0
+        c_better = 0
+        c_worse = 0
         equal = 0
-        almost_equal = 0
-        for i, ins in enumerate(insts):
+        for ins in insts:
             ins_id = ins[ins.rfind('_')+1:ins.find('.')]
+            if ins_id not in baselines:
+                continue
             out_file = "results/%s_%s" % (ins_id, config_str(config))
             if not os.path.isfile(out_file):
                 f.write('Erro: %s not exists\n' % out_file)
@@ -185,16 +155,85 @@ def test():
             total_cost = total_cost - dispatch * v_number
             total_cost = float(round(decimal.Decimal(total_cost), 2))
             if v_number < baselines[ins_id][0]:
-                better += 1
+                v_better += 1
             elif v_number > baselines[ins_id][0]:
-                worse += 1
-            elif total_cost == baselines[ins_id][1]:
+                v_worse += 1
+            elif total_cost < baselines[ins_id][1]:
+                c_better += 1
+            elif total_cost > baselines[ins_id][1]:
+                c_worse += 1
+            else:
                 equal += 1
-            elif abs(total_cost - baselines[ins_id][1]) == 0.01:
-                almost_equal += 1
-        f.write('%s, [%d, %d, %d, %d]\n' % (str(config, better, worse, equal, almost_equal)))
+        f.write('%s, [%d, %d, %d, %d, %d], [%d, %d, %d]\n' %
+                (str(config), v_better, v_worse, c_better, c_worse, equal,
+                 v_better+c_better, v_worse+c_worse, equal))
     f.close()
+
+# ('on', 0.75, 9)
+def config_str_1(L):
+    return "latin=%s_ksize=%s_elo=%s" %\
+           (L[0], L[1], L[2])
+
+def config_str(config):
+    return "latin=%s_ksize=%s_elo=%d" %\
+           (config[0], str(config[1]), config[2])
+
+def test():
+    solver = "LNSr/LNS/bin/LNS"
+    maxParalism = 14
+
+    runningTask = 0
+    processSet = set()
+    for config in grids:
+        print('--------------------Testing %s ----------------------' % str(config))
+        for i, ins in enumerate(insts):
+            print('------------------Testing instance %d---------------' % i)
+            while True:
+                if runningTask >= maxParalism:
+                    time.sleep(0.1)
+                    finished = [
+                        pid for pid in processSet if pid.poll() is not None
+                    ]
+                    processSet -= set(finished)
+                    runningTask = len(processSet)
+                    continue
+                else:
+                    ins_id = ins[ins.rfind('_')+1:ins.find('.')]
+                    with open(ins, 'r') as f:
+                        cus_num = int(f.read().strip().split('\n')[2].split(':')[1].strip())-1
+                        t = tmax[cus_num]
+                    out_file = "results/%s_%s" % (ins_id, config_str(config))
+                    cmd = ("%s --problem %s %s --pruning --O_1_eval --two_opt --two_opt_star "
+                           "--ksize %d --init %s "
+                           "--or_opt %d --two_exchange %d --random_removal --related_removal "
+                           "--regret_insertion --greedy_insertion "
+                           "--elo %d --random_seed %d --time %d --runs %d > %s") %\
+                            (solver, ins, '--latin' if config[0] == 'on' else '',
+                             math.floor(math.sqrt(cus_num)) if config[1] == 'sqrt' else\
+                             math.floor(config[1] * cus_num),
+                             init_list[0], or_opt_list[0], two_exchange_list[0], config[2],
+                             seeds[0], t, runs, out_file)
+                    print(cmd)
+                    processSet.add(subprocess.Popen(cmd, shell=True))
+                    # pid = subprocess.Popen(cmd, shell=True)
+                    # pid.communicate()
+                    runningTask = len(processSet)
+                    break
+
+    # check if subprocess all exits
+    while processSet:
+        time.sleep(5)
+        print('Still %d sub process not exits' % len(processSet))
+        finished = [pid for pid in processSet if pid.poll() is not None]
+        processSet -= set(finished)
 
 if __name__ == "__main__":
     os.chdir('../../')
-    test()
+    insts = glob("instances/Wang_Chen/explicit_*.vrpsdptw")
+    if sys.argv[1] == 'test':
+        test()
+    elif sys.argv[1] == 'summary':
+        summary()
+    elif sys.argv[1] == 'extract':
+        extract(sys.argv[2:])
+    os.chdir('LNSr/LNS/')
